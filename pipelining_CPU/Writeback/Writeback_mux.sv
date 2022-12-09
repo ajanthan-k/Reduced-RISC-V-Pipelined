412b755aad3db77ed9e7f61eda70a5959d866f3c
JimyhZhu <jimzhu0211@hotmail.com>
2022-12-09 14:28:12 +0000

    Writeback pipeline stage 1

---
+++ pipelining_CPU/Writeback/Writeback_mux.sv
@@ -0,0 +1,32 @@
+module Writeback_mux #(
+  parameter WIDTH = 32
+
+)(
+  //input logic    clk,
+  //input logic    rst,
+  input logic [WIDTH-1:0] ReadDataW,
+  input logic [WIDTH-1:0] PCPlus4W,
+  input logic [WIDTH-1:0] ALUResultM,
+
+  input logic [1:0] ResultSrcW,
+
+  output logic [WIDTH-1:0] ResultW
+
+
+);
+
+always_comb begin
+    
+        casez (ResultSrcW)
+
+            2'b00:  ResultW = ALUResultM; 
+            2'b01:  ResultW = ReadDataW;
+            2'b10:  ResultW = PCPlus4W;
+
+           default: ResultW = 0;
+    
+        endcase
+    
+     end
+
+endmodule
